4135ffc0bcd66ac1038a6cc015384cfc5e56149d
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-14 15:19:58 +0200

    Created controller structure

---
+++ controllers/topics.js
@@ -0,0 +1,16 @@
+exports.getTopic = (req, res, next) => {
+
+}
+
+exports.createTopic = (req, res, next) => {
+  
+}
+exports.updateTopic = (req, res, next) => {
+
+}
+exports.getTopic = (req, res, next) => {
+
+}
+exports.deleteTopic = (req, res, next) => {
+
+}
--- routes/topics.js
+++ routes/topics.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const {
+  getTopic,
+  getTopics,
+  createTopic,
+  updateTopic,
+  deleteTopic
+} = require("../controllers/topics");
 
-router.route('/').get(getTopics).post(createTopic)
+router
+  .route("/")
+  .get(getTopics)
+  .post(createTopic);
 
-router.route('/:id').get(getTopic).put(updateTopic).delete(deleteTopic)
-
+router
+  .route("/:id")
+  .get(getTopic)
+  .put(updateTopic)
+  .delete(deleteTopic);
 
 module.exports = router;

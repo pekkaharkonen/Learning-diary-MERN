a3e043af6f950606ff3ee088d63e2f600c995c40
Pekka Härkönen <pekka.o.harkonen@utu.fi>
2020-02-14 16:47:31 +0200

    controllers now using async/await consistently

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -9,39 +9,55 @@ const pool = new Pool({
 
 exports.getTopics = async (req, res, next) => {
   try {
-    let topics = await pool.query("SELECT * FROM topics");
+    let topics = await pool.query('SELECT * FROM topics');
     res.json({ success: true, data: topics });
   } catch (err) {
     next(err);
   }
 };
 
-exports.createTopic = (req, res, next) => {
-  const {title, description, startdate, inprogress} = req.body;
-  pool.query("INSERT INTO topics (title, description, startdate, inprogress) VALUES ($1, $2, $3, $4)", [title, description, startdate, inprogress])
-    .then(res.status(201).json({ success: true, data: [] }))
-    .catch(err => next(err))
-};
-
-exports.updateTopic = (req, res, next) => {
-  const {title, description, startdate, inprogress} = req.body;
-  pool.query("UPDATE topics SET title=$1, description=$2, startdate=$3, inprogress=$4 WHERE id= $5 ", [title, description, startdate, inprogress, req.params.id])
-    .then(res.status(200).json({success: true, data: []}))
-    .catch(err => next(err))
-};
-
-exports.getTopic = (req, res, next) => {
+exports.createTopic = async (req, res, next) => {
   try {
-    let topic = await pool.query("SELECT * FROM topics WHERE id=$1", [req.params.id])
-    res.status(200).json({success: true, data: topic}
-    )
+    const { title, description, startdate, inprogress } = req.body;
+    await pool.query(
+      'INSERT INTO topics (title, description, start_date, in_progress) VALUES ($1, $2, $3, $4)',
+      [title, description, startdate, inprogress]
+    );
+    res.status(201).json({ success: true, data: [] });
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
 
-exports.deleteTopic = (req, res, next) => {
-  pool.query("DELETE FROM topics WHERE id=$1", [req.params.id])
-    .then(res.status(200).json({success: true, data: []}))
-    .catch(err => next(err))
+exports.updateTopic = async (req, res, next) => {
+  try {
+    const { title, description, startdate, inprogress } = req.body;
+    await pool.query(
+      'UPDATE topics SET title=$1, description=$2, start_date=$3, in_progress=$4 WHERE id= $5 ',
+      [title, description, startdate, inprogress, req.params.id]
+    );
+    res.status(200).json({ success: true, data: [] });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTopic = async (req, res, next) => {
+  try {
+    let topic = await pool.query('SELECT * FROM topics WHERE id=$1', [
+      req.params.id
+    ]);
+    res.status(200).json({ success: true, data: topic });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteTopic = async (req, res, next) => {
+  try {
+    await pool.query('DELETE FROM topics WHERE id=$1', [req.params.id]);
+    res.status(200).json({ success: true, data: [] });
+  } catch (err) {
+    next(err);
+  }
 };
--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express();
 const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
+const topicsRouter = require('./routes/topics')
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('tiny'));
 }
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/topics', topicsRouter)
+
 
 const PORT = process.env.PORT || 3000;
 
